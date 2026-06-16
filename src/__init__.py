fafb71c38aec29bc96c90b4e7e2d7d98f4285563
adamez-f <adamez-f@student.42lyon.fr>
2026-06-16 19:56:02 +0200

    added: manager

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from src.function import FunctionDetails
 from src.parsing import Parsing
 from src.prompt import Prompt
+from src.manager import Manager
 
 
-__all__ = ["FunctionDetails", "Parsing", "Prompt"]
+__all__ = ["FunctionDetails", "Parsing", "Prompt", "Manager"]

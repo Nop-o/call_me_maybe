f749d54914c6ae300a2de120d5acc97dfa6b2fbb
adamez-f <adamez-f@student.42lyon.fr>
2026-06-23 17:46:45 +0200

    fix: handle crashes during dir creation

--- src/json_maker.py
+++ src/json_maker.py
@@ -1,5 +1,6 @@
 from .prompt import Prompt
 from .llm_manager import LlmManager
+from pathlib import Path
 import json
 
 
@@ -16,18 +17,38 @@ class JsonMaker:
             if data is None:
                 data = self.json_file_info
 
-            with open(file_name, 'w') as file:
-                file.write(json.dumps(data, indent=4))
+            try:
+                directory_path = Path.cwd() / "data/output"
+                directory_path.mkdir()
+            except FileExistsError:
+                pass
+
+            try:
+                with open(file_name, 'w') as file:
+                    file.write(json.dumps(data, indent=4))
+            except PermissionError as e:
+                print(e)
 
     def get_json_data(self, prompts: list[Prompt]) -> None:
         """Get json information based on given prompts"""
+        print("[")
         for prompt in prompts:
             answer: dict[str, str | dict[str, str]] = {}
 
+            print("\t{")
+
             answer["prompt"]: str = prompt.prompt
+            print(f'\t\t"prompt": "{answer["prompt"]}",')
+
             answer["name"]: str = self.manager.get_function_name(prompt.prompt)
+            print(f'\t\t"name": "{answer["name"]}",')
+
             answer["parameters"]: dict[str, str] = self.manager.get_parameters(
                 self.manager.find_function_from_name(answer["name"]),
                 prompt.prompt)
+            print(f'\t\t"parameters": {", ".join(answer["parameters"])}')
+
+            print("\t}")
 
             self.json_file_info.append(answer)
+        print("]")

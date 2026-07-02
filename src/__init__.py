006d1e028057145c0250f06f2b32bcfb834b0f85
adamez-f <adamez-f@student.42lyon.fr>
2026-07-02 06:11:38 +0200

    fix: combined two functions

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 from src.function import FunctionDetails
 from src.json_parsing import JsonParsing
-from src.prompt import Prompt, EncodedPrompt
+from src.prompt import Prompt, PromptEncoder
 from src.llm_manager import LlmManager
 from src.json_maker import JsonMaker
 
 
-__all__ = ["FunctionDetails", "JsonParsing", "Prompt", "EncodedPrompt",
+__all__ = ["FunctionDetails", "JsonParsing", "Prompt", "PromptEncoder",
            "LlmManager", "JsonMaker"]
--- src/prompt.py
+++ src/prompt.py
@@ -6,33 +6,12 @@ class Prompt(BaseModel):
     prompt: str = Field(min_length=1, max_length=200)
 
 
-class EncodedPrompt:
+class PromptEncoder:
 
-    def __init__(self) -> None:
-        self.model: Small_LLM_Model = Small_LLM_Model()
+    def __init__(self, model) -> None:
+        self.model: Small_LLM_Model = model
 
-    def encode_prompt_to_find_function_name(
-       self, system_tag: str, user_tag: str,
-       assistant_tag: str = "") -> list[int]:
-        """
-        Encode a prompt with a tag system to help the llm better understand
-        the received informations
-        """
-        return self.encode(
-f"""
-<|im_start|>system
-[
-    {system_tag}
-]
-<|im_end|>
-<|im_start|>user
-"{user_tag}"
-<|im_end|>
-<|im_start|>assistant
-{"{"}
-    {assistant_tag}""")
-
-    def encode_prompt_to_find_parameters(
+    def encode_prompt(
        self, system_tag: str, user_tag: str,
        assistant_tag: str = "", parameter_type="integer") -> list[int]:
         """
@@ -40,18 +19,15 @@ f"""
         the received informations
         """
         return self.encode(
-f"""
+            f"""
 <|im_start|>system
-{'{'}
 {system_tag}
-{'}'}
 <|im_end|>
 <|im_start|>user
 "{user_tag}"
 <|im_end|>
 <|im_start|>assistant
-{'{'}
-    {assistant_tag} {"\"" if parameter_type == "string" else ""}""")
+{assistant_tag}""")
 
     def decode(self, to_decode: list[int]) -> str:
         """Call the decode function from the llm"""

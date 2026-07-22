04e024317faeca43245f78a4be5cb29783cb7fac
adamez-f <adamez-f@student.42lyon.fr>
2026-07-22 12:01:32 +0200

    new: create flags file

---
+++ src/flags.py
@@ -0,0 +1,16 @@
+from typing import Any
+import argparse
+
+
+def get_flags() -> Any:
+    """Handle flags"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--functions_definition',
+                        default='data/input/functions_definition.json')
+    parser.add_argument('--input',
+                        default='data/input/function_calling_tests.json')
+    parser.add_argument('--output',
+                        default='data/output/function_calls.json')
+
+    return parser.parse_args()

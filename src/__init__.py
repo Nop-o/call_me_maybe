ab97061f8e75d0475cabb9a78616b74f92cf4735
adamez-f <adamez-f@student.42lyon.fr>
2026-07-22 11:58:03 +0200

    added: flags file import

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,8 @@ from src.json_parsing import JsonParsing
 from src.prompt import Prompt, PromptEncoder
 from src.llm_manager import LlmManager
 from src.json_maker import JsonMaker
+from src.flags import get_flags
 
 
 __all__ = ["FunctionDetails", "JsonParsing", "Prompt", "PromptEncoder",
-           "LlmManager", "JsonMaker"]
+           "LlmManager", "JsonMaker", "get_flags"]
